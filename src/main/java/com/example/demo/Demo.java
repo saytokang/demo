e2b98b258a98f8c2b09941f510706578f1f3d271
don hee <saytokang@gmail.com>
2025-07-05 15:31:24 +0900

    add Demo

---
+++ src/main/java/com/example/demo/Demo.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class Demo {
+    
+    @PostConstruct
+    private void init() {
+        log.info("start");
+    }
+}
